18bf517c3d1d0da2edadfd5dfab9ee0c592ef400
Fukutaro ABE <abfk01@gmail.com>
2022-08-04 15:50:21 +0000

    Initialize repository

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  
+  def hello
+    render html: "hello, world!"
+  end
+  
+  def goodbye
+    render html: "goodbye, world!"
+  end
+  
+  
+end
